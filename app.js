8d74a05be525667f0843ef5a92c4cc0740f51df1
joshuashea <joshshea44@gmail.com>
2021-06-21 10:34:45 -0700

    grid divs change color on mouseenter

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const game = () => {
   let rows = 16;
   const generateGrid = (columns, rows) => {
     const grid = document.querySelector('.container');
-    const boxes = columns * rows;
-    for (i = 0; i < boxes; i++) {
-      let box = document.createElement('div');
-      grid.appendChild(box);
+    const squares = columns * rows;
+    for (i = 0; i < squares; i++) {
+      const square = document.createElement('div');
+      grid.appendChild(square);
     }
     grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    const boxes = document.querySelectorAll('.container div');
+    boxes.forEach(box => {
+      box.addEventListener('mouseenter', () => {
+        const x = Math.floor(Math.random() * 256);
+        const y = Math.floor(Math.random() * 256);
+        const z = Math.floor(Math.random() * 256);
+        const bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
+        console.log(bgColor);
+
+        box.style.background = bgColor;
+      });
+    });
   };
   generateGrid(columns, rows);
 };
@@ -20,7 +32,7 @@ const game = () => {
 //   const bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
 //   console.log(bgColor);
 
-//   document.body.style.background = bgColor;
+//   document.div.style.background = bgColor;
 // }
 
 // random_bg_color();
