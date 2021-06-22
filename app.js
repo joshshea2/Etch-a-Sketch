5177156647954ea33dad228b63e809cd48d45022
joshuashea <joshshea44@gmail.com>
2021-06-21 18:47:40 -0700

    added html divs, css styling, options drop down menu

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+// entire game/webpage
 const game = () => {
-  let columns = 16;
+  // default dimensions
+  let columns = 32;
   let rows = 16;
+  // create divs based off dimensions.
   const generateGrid = (columns, rows) => {
-    const grid = document.querySelector('.container');
+    const grid = document.querySelector('.grid');
+    // how many divs
     const squares = columns * rows;
     for (i = 0; i < squares; i++) {
       const square = document.createElement('div');
+      // place divs on webpage
       grid.appendChild(square);
     }
+    // max divs for a column(so they're placed in a grid and not a line)
     grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-    const boxes = document.querySelectorAll('.container div');
+    const boxes = document.querySelectorAll('.grid div');
+    // for each div, make random color when you mouse over
     boxes.forEach(box => {
       box.addEventListener('mouseenter', () => {
         const x = Math.floor(Math.random() * 256);
@@ -23,18 +30,28 @@ const game = () => {
     });
   };
   generateGrid(columns, rows);
+
+  const dropdownOptions = () => {
+    const options = document.querySelector('.optionsBtn');
+    const optionsMenu = document.querySelector('.optionsMenu');
+    options.addEventListener('mouseover', () => {
+      optionsMenu.style.opacity = '50';
+      optionsMenu.style.pointerEvents = 'all';
+    });
+    optionsMenu.addEventListener('mouseover', () => {
+      optionsMenu.style.opacity = '50';
+      optionsMenu.style.pointerEvents = 'all';
+    });
+    options.addEventListener('mouseout', () => {
+      optionsMenu.style.opacity = '0';
+      optionsMenu.style.pointerEvents = 'none';
+    });
+    optionsMenu.addEventListener('mouseout', () => {
+      optionsMenu.style.opacity = '0';
+      optionsMenu.style.pointerEvents = 'none';
+    });
+  };
+  dropdownOptions();
 };
-
-// function random_bg_color() {
-//   const x = Math.floor(Math.random() * 256);
-//   const y = Math.floor(Math.random() * 256);
-//   const z = Math.floor(Math.random() * 256);
-//   const bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
-//   console.log(bgColor);
-
-//   document.div.style.background = bgColor;
-// }
-
-// random_bg_color();
 
 game();
