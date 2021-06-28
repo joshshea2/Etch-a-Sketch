230c32d9d78065013528e9db413bd19339a1b7e1
joshuashea <joshshea44@gmail.com>
2021-06-28 15:52:09 -0700

    changed option menu interaction

--- app.js
+++ app.js
@@ -7,52 +7,62 @@ const game = () => {
   // create divs based off dimensions.
   const generateGrid = (columns, rows) => {
     const grid = document.querySelector('.grid');
+    columns = 34;
+    rows = 16;
     // how many divs
     const squares = columns * rows;
 
     for (i = 0; i < squares; i++) {
       const square = document.createElement('div');
       square.setAttribute('id', 'pixel');
+      // square.style.background = white;
       // place divs on webpage
       grid.appendChild(square);
     }
 
     // max divs for a column(so they're placed in a grid and not a line)
     grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+  };
 
+  generateGrid(columns, rows);
+
+  const startPoint = () => {
     // for each div, make random color when you mouse over
     const boxes = document.querySelectorAll('.grid div');
 
     boxes.forEach(box => {
-      box.addEventListener('mouseenter', () => {
+      box.addEventListener('click', function divColor() {
         const x = Math.floor(Math.random() * 256);
         const y = Math.floor(Math.random() * 256);
         const z = Math.floor(Math.random() * 256);
         const bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
 
         box.style.background = bgColor;
+        console.log(this.id);
       });
     });
   };
-  generateGrid(columns, rows);
+  startPoint();
 
   // hover effect for options menu
   const dropdownOptions = () => {
     const options = document.querySelector('.optionsBtn');
     const optionsMenu = document.querySelector('.optionsMenu');
-    options.addEventListener('mouseover', () => {
+    const confirmBtn1 = document.querySelector('.confirmBtn1');
+    const confirmBtn2 = document.querySelector('.confirmBtn2');
+    options.addEventListener('click', () => {
       optionsMenu.style.opacity = '50';
       optionsMenu.style.pointerEvents = 'all';
     });
-    optionsMenu.addEventListener('mouseover', () => {
+    optionsMenu.addEventListener('mouseenter', () => {
       optionsMenu.style.opacity = '50';
       optionsMenu.style.pointerEvents = 'all';
     });
-    options.addEventListener('mouseout', () => {
+    confirmBtn1.addEventListener('click', () => {
       optionsMenu.style.opacity = '0';
       optionsMenu.style.pointerEvents = 'none';
     });
-    optionsMenu.addEventListener('mouseout', () => {
+    confirmBtn2.addEventListener('click', () => {
       optionsMenu.style.opacity = '0';
       optionsMenu.style.pointerEvents = 'none';
     });
@@ -60,7 +70,7 @@ const game = () => {
   dropdownOptions();
 
   // function to clear current sketch
-  const titlePress = document.querySelector('.title');
+  const titleBtn = document.querySelector('.title');
   const screen = document.querySelector('#grid');
 
   const clearScreen = parent => {
@@ -69,10 +79,11 @@ const game = () => {
     }
   };
 
-  // cleat current sketch when title is clicked
-  titlePress.addEventListener('click', () => {
+  // clear current sketch when title is clicked
+  titleBtn.addEventListener('click', () => {
     clearScreen(screen);
     generateGrid(columns, rows);
+    startPoint();
   });
 
   // clear current sketch && generate new grid from user input
@@ -86,6 +97,7 @@ const game = () => {
     getInput.addEventListener('click', () => {
       clearScreen(screen);
       generateGrid(columns, rows);
+      startPoint();
     });
   };
   newGrid();
